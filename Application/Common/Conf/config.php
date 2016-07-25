0049cc8815b219d46b803b4f21449c21e09dca49
1716771371@qq.com <1716771371@qq.com>
2016-07-25 11:27:57 +0800

    2

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -36,10 +36,10 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => '127.0.0.1', // 服务器地址
-    'DB_NAME'   => 'enter_web', // 数据库名
+    'DB_HOST'   => '10.1.56.115', // 服务器地址
+    'DB_NAME'   => 'enter_wc', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root',  // 密码
+    'DB_PWD'    => 'zhaojun',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'dev_', // 数据库表前缀
 
--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', '^Tir".oa3l@*Z!LMvmn2q<jxCIXR>s$)#%;bHAKQ'); //加密KEY
-define('UC_DB_DSN', 'mysql://root:root@127.0.0.1:3306/enter_web'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysql://root:zhaojun@10.1.56.115:3306/enter_web'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'dev_'); // 数据表前缀，使用Model方式调用API必须配置此项
