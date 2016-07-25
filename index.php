3b384e43830730efb9c329325033631874a2d877
1716771371@qq.com <1716771371@qq.com>
2016-07-25 11:35:06 +0800

    2

--- index.php
+++ index.php
@@ -7,30 +7,30 @@
 // | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
 // +----------------------------------------------------------------------
 
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+if (version_compare(PHP_VERSION, '5.3.0', '<')) die('require PHP > 5.3.0 !');
 
 /**
  * 系统调试设置
  * 项目正式部署后请设置为false
  */
-define('APP_DEBUG', true );
+define('APP_DEBUG', true);
 
 /**
  * 应用目录设置
  * 安全期间，建议安装调试完成后移动到非WEB目录
  */
-define ( 'APP_PATH', './Application/' );
+define('APP_PATH', './Application/');
 
-if(!is_file(APP_PATH . 'User/Conf/config.php')){
-	header('Location: ./install.php');
-	exit;
+if (!is_file(APP_PATH . 'User/Conf/config.php')) {
+    header('Location: ./install.php');
+    exit;
 }
 
 /**
  * 缓存目录设置
  * 此目录必须可写，建议移动到非WEB目录
  */
-define ( 'RUNTIME_PATH', './Runtime/' );
+define('RUNTIME_PATH', './Runtime/');
 
 /**
  * 引入核心入口
